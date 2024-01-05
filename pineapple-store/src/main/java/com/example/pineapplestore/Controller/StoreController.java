9ca5c76f892a71dc7ec9bc866049b73c473e2fff
Mahinr10 <mahinr369@gmail.com>
2024-01-05 15:32:59 +0600

    added rest connection between store and inventory

--- pineapple-store/src/main/java/com/example/pineapplestore/Controller/StoreController.java
+++ pineapple-store/src/main/java/com/example/pineapplestore/Controller/StoreController.java
@@ -2,6 +2,9 @@ package com.example.pineapplestore.Controller;
 
 import com.example.pineapplestore.Entity.PineappleBatch;
 import com.example.pineapplestore.feignClient.InventoryService;
+import feign.FeignException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +23,7 @@ public class StoreController {
 
     @GetMapping("/get-inventory")
     public ResponseEntity<List<PineappleBatch>> getAllPineappleBatch() {
-        return new ResponseEntity<>(inventoryService.getAll(), HttpStatus.FOUND);
+        return inventoryService.getAll();
     }
 
 }
--- pineapple-store/src/main/java/com/example/pineapplestore/feignClient/InventoryService.java
+++ pineapple-store/src/main/java/com/example/pineapplestore/feignClient/InventoryService.java
@@ -2,6 +2,7 @@ package com.example.pineapplestore.feignClient;
 
 import com.example.pineapplestore.Entity.PineappleBatch;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -9,5 +10,5 @@ import java.util.List;
 @FeignClient(name = "pineapple-inventory")
 public interface InventoryService {
     @RequestMapping("/api/inventory/get-all")
-    List<PineappleBatch> getAll();
+    ResponseEntity<List<PineappleBatch>> getAll();
 }
