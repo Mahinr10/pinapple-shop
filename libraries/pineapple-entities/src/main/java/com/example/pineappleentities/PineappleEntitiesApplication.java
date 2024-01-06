f09f0e6fde4f651a374675521e2e996a857f3f87
Mahinr10 <mahinr369@gmail.com>
2024-01-06 10:59:07 +0600

    refactored dirctories

---
+++ libraries/pineapple-entities/src/main/java/com/example/pineappleentities/PineappleEntitiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.pineappleentities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PineappleEntitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PineappleEntitiesApplication.class, args);
+	}
+
+}
---
+++ libraries/pineapple-entities/src/test/java/com/example/pineappleentities/PineappleEntitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pineappleentities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PineappleEntitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
